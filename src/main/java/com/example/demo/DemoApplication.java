5b2ba4b83c17c99bd368987d552da2125cfe1231
xiaofuyang <1004589810@qq.com>
2018-10-16 16:54:02 +0800

    idea 整合

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoApplication {
     @RequestMapping("/")
     String index(){
-        return "Hello Spring Boot";
+        return "Hello Spring ddddBoot";
     }
 
 
